91c2579a2c9213d353cb4d3ec1e18179f4e2983a
Rene-Cruz <cv5676@innovaccion.mx>
2021-08-01 07:47:33 -0500

    Adding new files

---
+++ AppPatrulla/Models/Control.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppPatrulla.Models
+{
+    public class Control
+    {
+        public string Cedula { get; set; }
+        public string Nombres { get; set; }
+        public string Apellido { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+
+
+    }
+}
---
+++ AppPatrulla/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppPatrulla.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
